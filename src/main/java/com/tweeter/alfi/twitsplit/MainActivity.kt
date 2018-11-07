04a2cc690699fc6111551ce04b39d509a0e114b4
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-11-07 21:33:03 +0700

    Update file
    Modifying mainactivity file

--- src/main/java/com/tweeter/alfi/twitsplit/MainActivity.kt
+++ src/main/java/com/tweeter/alfi/twitsplit/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.tweeter.alfi.twitsplit
 
+import android.R.layout.simple_list_item_1
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.ceil
-import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,19 +16,65 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun String.splitString(size: Int): List<String> {
-        val nSplits = ceil(length.toDouble() / size.toDouble())
-        val nSplitsInt = nSplits.toInt()
+    fun errorToast() {
+        val myToast = Toast.makeText(this, "Error! Your sentence contains a span \n" +
+                "of non-whitespace characters longer than 50 characters!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun String.splitMessage(size: Int): List<String> {
+
+        val nSplitsInt = ceil(length.toDouble() / size.toDouble()).toInt()
         println("length: " + length)
-        println("nSplits: " + nSplitsInt)
-//        return (0 until nSplitsInt).map { substring(it * size, (it + 1) * size)}
-          return (0 until nSplitsInt).map {substring(it * size, minOf(length, (it + 1) * size))}
+        println("nSplitsInt: " + nSplitsInt)
+
+        val listSplitString  = (0 until nSplitsInt).map { val partIndicator = """${(it + 1).toString()}/$nSplitsInt """
+
+            var partIndicatorLength = partIndicator.length
+            val startOfEachSplit = (it * size) - (it * partIndicatorLength)
+            val endOfEachSplit = ((it + 1) * size) - ((it + 1) * partIndicatorLength)
+            val substringToSplit = substring(startOfEachSplit, minOf(length, endOfEachSplit))
+            val errorMessage = "ERROR!!Non-whitespace!"
+
+            if (substringToSplit.contains("\\s".toRegex())) {
+                println("hohoho")
+                val eachOfString: String = """$partIndicator ${
+                    substringToSplit
+                }"""
+
+                eachOfString
+            }
+            else {
+                if (it + 1 != nSplitsInt) {
+                    errorMessage
+                } else {
+                    val eachOfString: String = """$partIndicator ${
+                    substringToSplit
+                    }"""
+
+                    eachOfString
+                }
+            }
+
+        }
+
+        println(listSplitString)
+        return listSplitString
     }
 
     fun tweetSubmit(view: View) {
         val postString = plain_text_input.text.toString()
-        val splitPost = postString.splitString(50)
-        postTextView.text = splitPost.toString()
+        val splitPost = postString.splitMessage(50)
+        val splitPostLastString = splitPost.lastIndex
+        val messages= splitPost.map{
+            if (splitPost.indexOf(it) != splitPostLastString) {
+                it.substringBeforeLast(" ")
+            } else {
+                it
+            }
+        }
+
+        lv_messages.adapter = ArrayAdapter(this, simple_list_item_1, messages)
     }
 
 }
